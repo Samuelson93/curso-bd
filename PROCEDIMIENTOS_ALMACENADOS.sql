6834408a4fb74974511c101312775a5a3c287387
Samuelra93 <samuel_ra93@hotmail.com>
2017-06-02 19:43:53 +0100

    Creamos la nueva consulta y ordenamos por la marca los coches

--- PROCEDIMIENTOS_ALMACENADOS.sql
+++ PROCEDIMIENTOS_ALMACENADOS.sql
@@ -2,14 +2,28 @@
 ALTER PROCEDURE GET_COCHE_POR_MARCA
 AS
 BEGIN
-	SELECT  count(*) FROM Coches
-	SELECT *  FROM Coches
-	INSERT INTO Coches (matricula, idMarca, idTiposCombustible,
-				color, cilindrada, nPlazas, fechaMatriculacion)
-	SELECT  matricula, idMarca, idTiposCombustible, color,
-			cilindrada, nPlazas, fechaMatriculacion  
-	FROM Coches	
+	--SELECT  count(*) FROM Coches
+	--SELECT *  FROM Coches
+	--INSERT INTO Coches (matricula, idMarca, idTiposCombustible,
+	--			color, cilindrada, nPlazas, fechaMatriculacion)
+	--SELECT  matricula, idMarca, idTiposCombustible, color,
+	--		cilindrada, nPlazas, fechaMatriculacion  
+	--FROM Coches	
 
-	SELECT * FROM Coches
+	SELECT 
+	  Marcas.denominacion as denominacionMarca
+	, TiposCombustible.denominacion as denominacionTipoCombustible
+	, Coches.id, Coches.matricula, Coches.color, Coches.nPlazas
+	, Coches.fechaMatriculacion, coches.cilindrada, coches.idMarca, coches.idTiposCombustible  
+	
+	FROM Marcas
+		INNER JOIN Coches on Marcas.id = Coches.idMarca
+		INNER JOIN TiposCombustible on Coches.idTiposCombustible = TiposCombustible.id
+	GROUP BY
+	Marcas.denominacion 
+	, TiposCombustible.denominacion 
+	, Coches.id, Coches.matricula, Coches.color, Coches.nPlazas
+	, Coches.fechaMatriculacion, coches.cilindrada, coches.idMarca, coches.idTiposCombustible 
+	ORDER BY Marcas.denominacion 
 	--PRINT 'MI PRIMER PROCEDIMIENTO ALMACENADO'
 END
